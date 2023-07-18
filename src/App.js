f7ea93748dbc52a046b44081b0f89861f7714a4d
Dang The Anh <anhndt@fanxipan.net>
2023-07-18 21:59:37 +0700

    update code 18/7

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(60);
-
-  let timerId = useRef();
-  console.log(timerId.current);
-  const handleStart = () => {
-    timerId.current = setInterval(() => {
-      setCount((prev) => prev - 1);
-    }, 1000);
-  };
-  const handleStop = () => {
-    clearInterval(timerId.current);
-  };
-  return (
-    <div className="App">
-      <p>{count}</p>
-      <div>
-        <button onClick={handleStart}>Start</button>
-        <button onClick={handleStop}>Stop</button>
-      </div>
-    </div>
-  );
+  return <div className="App">My Tiktok app</div>;
 }
 
 export default App;
